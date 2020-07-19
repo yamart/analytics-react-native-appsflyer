bbb8a4e2830de89b928dc3c8be5fb609a37dfa44
Yaman Al-Kamaly <yaman.al-kamaly@finder.com>
2020-07-20 04:48:53 +1000

    test: move to java

---
+++ android/src/main/java/com/segment/analytics/reactnative/integration/appsflyer/RNAnalyticsIntegration_AppsFlyerModule.java
@@ -0,0 +1,44 @@
+package com.segment.analytics.reactnative.integration.appsflyer;
+
+import android.util.Log;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.segment.analytics.reactnative.core.RNAnalytics;
+import com.segment.analytics.android.integrations.appsflyer.AppsflyerIntegration;
+import com.facebook.react.modules.core.DeviceEventManagerModule.RCTDeviceEventEmitter;
+
+import java.util.Map;
+
+public class RNAnalyticsIntegration_AppsFlyerModule extends ReactContextBaseJavaModule {
+
+  private final ReactApplicationContext reactContext;
+
+  public RNAnalyticsIntegration_AppsFlyerModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
+
+  @Override
+  public String getName() {
+    return "RNAnalyticsIntegration_AppsFlyer";
+  }
+
+  @ReactMethod
+  public void setup() {
+    RNAnalytics.INSTANCE.addIntegration(AppsflyerIntegration.FACTORY);
+    this.initConversionListener();
+  }
+
+  private void initConversionListener() {
+    final RNAnalyticsIntegration_AppsFlyerModule self = this;
+
+    AppsflyerIntegration.cld = new AppsflyerIntegration.ConversionListenerDisplay() {
+      @Override
+      public void display(Map<String, String> attributionData) {
+        self.reactContext.getJSModule(RCTDeviceEventEmitter.class).emit("onInstallConversionData", attributionData.toString());
+      }
+    };
+  }
+}
