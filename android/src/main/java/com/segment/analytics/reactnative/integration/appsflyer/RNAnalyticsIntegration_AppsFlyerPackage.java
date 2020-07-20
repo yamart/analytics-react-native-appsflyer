003e49a406fd5ddb586e3f91b75b85608b801e0b
Yaman Al-Kamaly <yaman.al-kamaly@finder.com>
2020-07-20 13:05:18 +1000

    test: move to java

---
+++ android/src/main/java/com/segment/analytics/reactnative/integration/appsflyer/RNAnalyticsIntegration_AppsFlyerPackage.java
@@ -0,0 +1,23 @@
+package com.segment.analytics.reactnative.integration.appsflyer;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.bridge.JavaScriptModule;
+
+public class RNAnalyticsIntegration_AppsFlyerPackage implements ReactPackage {
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+    return Arrays.<NativeModule>asList(new RNAnalyticsIntegration_AppsFlyerModule(reactContext));
+  }
+
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+    return Collections.emptyList();
+  }
+}
